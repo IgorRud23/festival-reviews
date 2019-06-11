c2f8b2527d75adeb72aed613192d2ce0506695e9
IgorRud23 <rud.igor23@gmail.com>
2019-06-10 20:37:18 -0400

    init views signup, login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   get '/login' do
-    erb :"session/login"
-    
+    erb :"users/login"
+  end
+
+  get '/signup' do
+    erb :"users/signup"
   end
 end
