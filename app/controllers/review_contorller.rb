544855ebde255aed4b834f6d931db5464a6cb9b0
IgorRud23 <rud.igor23@gmail.com>
2019-06-16 21:37:06 -0400

    fixed new review

--- app/controllers/review_contorller.rb
+++ app/controllers/review_contorller.rb
@@ -10,8 +10,8 @@ class ReviewController < ApplicationController
 
   post '/reviews/new/:festival_id' do
     if true
-      @review = Review.new(params)
-      @review.save
+      @festival = Festival.find(params[:festival_id])
+      Review.create(content: params[:content], festival_id: params[:festival_id], user_id: current_user.id)
       redirect "/festivals/#{params[:festival_id]}"
     else
       flash[:review_error] = "Review should be more than two symbols"
